7c347f52fb7d4d768360a9a56405a7dfdd67c959
alexxx888 <alexxxusachev888@gmail.com>
2023-05-03 15:32:53 +0300

    working cli app for managing contacts with readme file and snapshots

---
+++ README.md
@@ -0,0 +1,18 @@
+
+# Node.js Command Line Contacts App
+This Node.js command line app allows you to manage a list of contacts, which includes adding, removing, and searching for contacts. The contacts data is stored in a JSON file (contacts.json), making it easy to manage and access your contacts.
+[Homework delivery](https://monosnap.com/list/6452513e9c43766059dae492)
+
+## Features
+- List all contacts
+- Get a contact by ID
+- Add a new contact
+- Remove a contact by ID
+
+## Usage
+The application supports the following commands:
+
+- action list: List all contacts
+- action get --id [ID]: Get a contact by its ID
+- action add --name [NAME] --email [EMAIL] --phone [PHONE]: Add a new contact with the specified name, email, and phone number
+- action remove --id [ID]: Remove a contact by its ID
---
+++ db/contacts.js
@@ -0,0 +1,59 @@
+const fs = require('fs/promises');
+const path = require('path');
+const {nanoid } = require("nanoid");
+
+const contactsPath = path.join(__dirname, 'contacts.json');
+
+async function listContacts() {
+    const getAllContacts = await fs.readFile(contactsPath);
+    return JSON.parse(getAllContacts);
+}
+
+async function getContactById(contactId) {
+    const contactsList = await listContacts();
+    const foundContact = contactsList.find(contact=> contact.id === contactId)
+    return foundContact || null;
+}
+
+async  function removeContact(contactId) {
+    const contactsList = await listContacts();
+    const contactIndex = contactsList.findIndex(contact => contact.id === contactId);;
+    if(contactIndex === -1){
+        return null;
+    }
+
+    const [removedContact] = contactsList.splice(contactIndex, 1);
+    await fs.writeFile(contactsPath,JSON.stringify(contactsList, null, 2));
+    return removedContact;
+
+    /* const contactsList = await listContacts();
+    const removedContact = contactsList.find(contact => contact.id === contactId);
+
+    if (!removedContact) {
+        return null;
+    }
+
+    const filteredContactsList = contactsList.filter(contact => contact.id !== contactId);
+    await fs.writeFile(contactsPath, JSON.stringify(filteredContactsList, null, 2));
+    return removedContact; */
+}
+
+async function addContact(name, email, phone) {
+    const contactsList = await listContacts();
+    const newContact = {
+        id: nanoid(),
+        name,
+        email,
+        phone
+    }
+    contactsList.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contactsList, null, 2))
+    return newContact;
+}
+
+  module.exports = {
+    listContacts, 
+    getContactById, 
+    removeContact,
+    addContact
+  }
